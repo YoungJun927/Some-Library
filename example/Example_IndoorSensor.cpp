ffa018de2d20c91e261bb29fa09547165df25084
YoungJun <dudwns927@gmail.com>
2018-01-25 12:13:08 +0900

    Fixed data size and  refactoring

--- example/Example_IndoorSensor.cpp
+++ example/Example_IndoorSensor.cpp
@@ -1,15 +1,23 @@
 #include <cox.h>
-#include "LoRaMacKR920.hpp"
 #include "PMS3003.hpp"
 #include "RHT03.hpp"
 #include "SparkFunCCS811.hpp"
+#include "LoRaMacKR920.hpp"
 
 PMS3003 pms3003 = PMS3003(Serial2, D3, D4);
 CCS811 mysensor(Wire, 0x5B);
 RHT03 rht;
-LoRaMacKR920 LoRaWAN = LoRaMacKR920(SX1276, 10);
 
-Timer test;
+#define LORAWAN_SKT 0
+#if (LORAWAN_SKT == 1)
+#include "LoRaMacKR920SKT.hpp"
+LoRaMacKR920SKT LoRaWAN = LoRaMacKR920SKT(SX1276, 10);
+#else
+#include "LoRaMacKR920.hpp"
+LoRaMacKR920 LoRaWAN = LoRaMacKR920(SX1276, 10);
+#endif
+
+Timer measurement;
 Timer timerSend;
 
 int32_t temp1_0=0;
@@ -26,17 +34,18 @@ float latestTempF;
 
 #if (OVER_THE_AIR_ACTIVATION == 1)
 static uint8_t devEui[]="\x14\x0C\x5B\xFF\xFF\x00\x05\x4A";
+static uint8_t appKey[]="\x4e\x59\x36\xca\x46\x05\x32\xec\xe8\x50\xeb\xd0\x0b\x75\xef\x25";
 static const uint8_t appEui[] = "\x00\x00\x00\x00\x00\x00\x00\x00";
-static uint8_t appKey[16];
 
 char keyBuf[128];
 Timer timerKeyInput;
 #else
 
-static const uint8_t NwkSKey[] = "\x72\xcc\xac\xba\xfe\x7f\x25\x69\x77\xcb\xcc\xb9\xad\x97\xde\x02";
-static const uint8_t AppSKey[] = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+static const uint8_t NwkSKey[] = "\xa4\x88\x55\xad\xe9\xf8\xf4\x6f\xa0\x94\xb1\x98\x36\xc3\xc0\x86";
+static const uint8_t AppSKey[] = "\x7a\x56\x2a\x75\xd7\xa3\xbd\x89\xa3\xde\x53\xe1\xcf\x7f\x1c\xc7";
 static uint32_t DevAddr = 0x06e632e8;
 #endif //OVER_THE_AIR_ACTIVATION
+
 static void eventSensorMeasuredDone(int32_t pm1_0_CF1,
                                     int32_t pm2_5_CF1,
                                     int32_t pm10_0_CF1,
@@ -57,7 +66,7 @@ static void taskPeriodicSend(void *) {
 
   f->port = 1;
   f->type = LoRaMacFrame::CONFIRMED;
-  f->len = sprintf((char *) f->buf,"\"Humidity\":\"%u.%u\",\"Temp(C)\":\"%u.%u\",\"PM1_0\":\"%lu\",\"PM10_0\":\"%lu\",\"CO2\":\"%d\",``\"TVOC\":\"%d\"",
+  f->len = sprintf((char *) f->buf,"\"Hum\":%u.%u,\"Tem\":%u.%u,\"PM1\":%lu,\"PM10\":%lu,\"CO2\":%d,\"VOC\":%d",
                                       (uint16_t) latestHumidity,
                                       (uint16_t) round(latestHumidity * 100) % 100,
                                       (uint16_t) latestTempC,
@@ -66,17 +75,22 @@ static void taskPeriodicSend(void *) {
                                       temp10_0,
                                       CO2,
                                       TVOC);
+
   error_t err = LoRaWAN.send(f);
   printf("* Sending periodic report (%s (%u byte)): %d\n", f->buf, f->len, err);
   if (err != ERROR_SUCCESS) {
     delete f;
-    timerSend.startOneShot(600000);
+    timerSend.startOneShot(10000);
   }
+
   err = LoRaWAN.requestLinkCheck();
   printf("* Request LinkCheck: %d\n", err);
+
+  // err = LoRaWAN.requestDeviceTime();
+  // printf("* Request DeviceTime: %d\n", err);
 }
-//! [How to send]
-static void air(void*){
+
+static void readSensor(void*){
   digitalWrite(D4,HIGH);
   int updateRet = rht.update();
   if (updateRet == 1){
@@ -89,8 +103,12 @@ static void air(void*){
   TVOC=mysensor.getTVOC();
   pms3003.onReadDone(eventSensorMeasuredDone);
   digitalWrite(D4,LOW);
-  }
+}
+//! [How to send]
 
+#if (OVER_THE_AIR_ACTIVATION == 1)
+
+#if (LORAWAN_SKT == 1)
 //! [How to use onJoin callback for SKT]
 static void eventLoRaWANJoin(
   LoRaMac &,
@@ -101,28 +119,57 @@ static void eventLoRaWANJoin(
   const uint8_t *joinedNwkSKey,
   const uint8_t *joinedAppSKey,
   uint32_t joinedDevAddr,
-  const RadioPacket &) {
-#if (OVER_THE_AIR_ACTIVATION == 1)
-  if (joined) {
-    if (joinedNwkSKey && joinedAppSKey) {
-      // Status is OK, node has joined the network
-      printf("* Joined to the network!\n");
+  const RadioPacket &
+) {
+  if (joinedNwkSKey && joinedAppSKey) {
+    /* RealAppKey Joining */
+    if (joined) {
+      Serial.println("* RealAppKey joining done!");
       postTask(taskPeriodicSend, NULL);
     } else {
-      printf("* PseudoAppKey joining done!\n");
+      Serial.println("* RealAppKey joining failed. Retry to join.");
+      LoRaWAN.beginJoining(NULL, NULL, NULL);
     }
   } else {
-    printf("* Join failed. Retry to join\n");
-    LoRaWAN.beginJoining(devEui, appEui, appKey);
+    /* PseudoAppKey Joining */
+    if (joined) {
+      Serial.println("* PseudoAppKey joining done! Let's do RealAppKey joining!");
+      LoRaWAN.beginJoining(NULL, NULL, NULL);
+    } else {
+      Serial.println("* PseudoAppKey joining failed. Retry to join.");
+      LoRaWAN.beginJoining(devEui, appEui, appKey);
+    }
   }
-#endif
 }
 //! [How to use onJoin callback for SKT]
+#else
+//! [How to use onJoin callback]
+static void eventLoRaWANJoin(
+  LoRaMac &lw,
+  bool joined,
+  const uint8_t *joinedDevEui,
+  const uint8_t *joinedAppEui,
+  const uint8_t *joinedAppKey,
+  const uint8_t *joinedNwkSKey,
+  const uint8_t *joinedAppSKey,
+  uint32_t joinedDevAddr,
+  const RadioPacket &
+) {
+  if (joined) {
+    Serial.println("* Joining done!");
+    postTask(taskPeriodicSend, NULL);
+  } else {
+    Serial.println("* Joining failed. Retry to join.");
+    lw.beginJoining(devEui, appEui, appKey);
+  }
+}
+//! [How to use onJoin callback]
+#endif //LORAWAN_SKT
+#endif //OVER_THE_AIR_ACTIVATION
 
 //! [How to use onSendDone callback]
-
-static void eventLoRaWANSendDone(LoRaMac &, LoRaMacFrame *frame) {
-  printf(
+static void eventLoRaWANSendDone(LoRaMac &lw, LoRaMacFrame *frame) {
+  Serial.printf(
     "* Send done(%d): destined for port:%u, fCnt:0x%08lX, Freq:%lu Hz, "
     "Power:%d dBm, # of Tx:%u, ",
     frame->result,
@@ -140,24 +187,26 @@ static void eventLoRaWANSendDone(LoRaMac &, LoRaMacFrame *frame) {
     if (frame->meta.LoRa.bw > 3) {
       frame->meta.LoRa.bw = (Radio::LoRaBW_t) 4;
     }
-    printf("LoRa, SF:%u, BW:%s, ", frame->meta.LoRa.sf, strBW[frame->meta.LoRa.bw]);
+    Serial.printf(
+      "LoRa, SF:%u, BW:%s, ", frame->meta.LoRa.sf, strBW[frame->meta.LoRa.bw]
+    );
   } else if (frame->modulation == Radio::MOD_FSK) {
-    printf("FSK, ");
+    Serial.printf("FSK, ");
   } else {
-    printf("Unkndown modulation, ");
+    Serial.printf("Unkndown modulation, ");
   }
   if (frame->type == LoRaMacFrame::UNCONFIRMED) {
-    printf("UNCONFIRMED");
+    Serial.printf("UNCONFIRMED");
   } else if (frame->type == LoRaMacFrame::CONFIRMED) {
-    printf("CONFIRMED");
+    Serial.printf("CONFIRMED");
   } else if (frame->type == LoRaMacFrame::MULTICAST) {
-    printf("MULTICAST (error)");
+    Serial.printf("MULTICAST (error)");
   } else if (frame->type == LoRaMacFrame::PROPRIETARY) {
-    printf("PROPRIETARY");
+    Serial.printf("PROPRIETARY");
   } else {
-    printf("unknown type");
+    Serial.printf("unknown type");
   }
-  printf(" frame\n");
+  Serial.printf(" frame\n");
 
   for (uint8_t t = 0; t < 8; t++) {
     const char *strTxResult[] = {
@@ -167,41 +216,49 @@ static void eventLoRaWANSendDone(LoRaMac &, LoRaMacFrame *frame) {
       "air busy",
       "Tx timeout",
     };
-    printf("- [%u] %s\n", t, strTxResult[min(frame->txResult[t], 4)]);
+    Serial.printf("- [%u] %s\n", t, strTxResult[min(frame->txResult[t], 4)]);
   }
   delete frame;
 
-  timerSend.startOneShot(300000);
+  timerSend.startOneShot(60000);
 }
 //! [How to use onSendDone callback]
 
 //! [How to use onReceive callback]
-static void eventLoRaWANReceive(LoRaMac &, const LoRaMacFrame *frame) {
-  printf(
-    "* Received: destined for port:%u, fCnt:0x%08lX, Freq:%lu Hz, RSSI:%d dB",
-    frame->port,
-    frame->fCnt,
-    frame->freq,
-    frame->power
-  );
+static void eventLoRaWANReceive(LoRaMac &lw, const LoRaMacFrame *frame) {
+  static uint32_t fCntDownPrev = 0;
+
+  Serial.print("* Received a frame. Destined for port:");
+  Serial.print(frame->port);
+  Serial.print(", fCnt:0x");
+  Serial.print(frame->fCnt, HEX);
+  if (fCntDownPrev != 0 && fCntDownPrev == frame->fCnt) {
+    Serial.print(" (duplicate)");
+  }
+  fCntDownPrev = frame->fCnt;
+  Serial.print(", Freq:");
+  Serial.print(frame->freq);
+  Serial.print(" Hz, RSSI:");
+  Serial.print(frame->power);
+  Serial.print(" dB");
 
   if (frame->modulation == Radio::MOD_LORA) {
     const char *strBW[] = {
       "Unknown", "125kHz", "250kHz", "500kHz", "Unexpected value"
     };
-    printf(
+    Serial.printf(
       ", LoRa, SF:%u, BW:%s",
       frame->meta.LoRa.sf, strBW[min(frame->meta.LoRa.bw, 4)]
     );
   } else if (frame->modulation == Radio::MOD_FSK) {
-    printf(", FSK");
+    Serial.printf(", FSK");
   } else {
-    printf(", Unkndown modulation");
+    Serial.printf(", Unkndown modulation");
   }
   if (frame->type == LoRaMacFrame::UNCONFIRMED) {
-    printf(", Type:UNCONFIRMED,");
+    Serial.printf(", Type:UNCONFIRMED");
   } else if (frame->type == LoRaMacFrame::CONFIRMED) {
-    printf(", Type:CONFIRMED,");
+    Serial.printf(", Type:CONFIRMED");
 
     if (LoRaWAN.getNumPendingSendFrames() == 0) {
       // If there is no pending send frames, send an empty frame to ack.
@@ -215,17 +272,20 @@ static void eventLoRaWANReceive(LoRaMac &, const LoRaMacFrame *frame) {
     }
 
   } else if (frame->type == LoRaMacFrame::MULTICAST) {
-    printf(", Type:MULTICAST,");
+    Serial.printf(", Type:MULTICAST");
   } else if (frame->type == LoRaMacFrame::PROPRIETARY) {
-    printf(", Type:PROPRIETARY,");
+    Serial.printf(", Type:PROPRIETARY");
   } else {
-    printf(", unknown type,");
+    Serial.printf(", unknown type");
   }
 
-  for (uint8_t i = 0; i < frame->len; i++) {
-    printf(" %02X", frame->buf[i]);
+  if (frame->len > 0) {
+    Serial.println(", ");
+    for (uint8_t i = 0; i < frame->len; i++) {
+      Serial.printf(" %02X", frame->buf[i]);
+    }
   }
-  printf("\n");
+  Serial.println();
 }
 //! [How to use onReceive callback]
 
@@ -285,15 +345,25 @@ static void printChannelInformation(LoRaMac &lw) {
   }
   //! [getDatarate]
 
+  //! [getTxPower]
+  int8_t power = lw.getTxPower(lw.getCurrentTxPowerIndex());
+  printf(" - Default Tx: ");
+  if (power == -127) {
+    printf("unexpected value\n");
+  } else {
+    printf("%d dBm\n", power);
+  }
+  //! [getTxPower]
+
   printf(
     " - # of repetitions of unconfirmed uplink frames: %u\n",
     lw.getNumRepetitions()
   );
 }
 
-static void eventLoRaWANLinkADRAnsSent(LoRaMac &l, uint8_t status) {
+static void eventLoRaWANLinkADRAnsSent(LoRaMac &lw, uint8_t status) {
   printf("* LoRaWAN LinkADRAns sent with status 0x%02X.\n", status);
-  printChannelInformation(l);
+  printChannelInformation(lw);
 }
 //! [eventLoRaWANLinkADRAnsSent]
 
@@ -462,55 +532,94 @@ static void eventLoRaWANDeviceTimeAnswered(
 }
 //! [How to use onDeviceTimeAnswered callback]
 
+static void eventButtonPressed() {
+  printf("* Button pressed:\n");
+
+  LoRaMacFrame *f = new LoRaMacFrame(20);
+  if (f == NULL) {
+    printf("- Not enough memory\n");
+    return;
+  }
+
+  if (LoRaWAN.getDeviceClass() == LoRaMac::CLASS_A) {
+    printf("- Change class A to C\n");
+    f->len = sprintf((char *) f->buf, "\"class\":\"C\"");
+  } else {
+    printf("- Change class C to A\n");
+    f->len = sprintf((char *) f->buf, "\"class\":\"A\"");
+  }
+
+  f->port = 223;
+  f->type = LoRaMacFrame::CONFIRMED;
+  error_t err = LoRaWAN.send(f);
+  printf(
+    "* Sending class configuration message (%s (%u byte)): %d\n",
+    f->buf, f->len, err
+  );
+  if (err == ERROR_SUCCESS) {
+    if (LoRaWAN.getDeviceClass() == LoRaMac::CLASS_A) {
+      LoRaWAN.setDeviceClass(LoRaMac::CLASS_C);
+    } else {
+      LoRaWAN.setDeviceClass(LoRaMac::CLASS_A);
+    }
+  } else {
+    delete f;
+  }
+}
+
 #if (OVER_THE_AIR_ACTIVATION == 1)
 
+#if (LORAWAN_SKT == 1)
 static void taskBeginJoin(void *) {
   Serial.stopListening();
-  Serial.println("* Let's start join!");
 
 #if 0
+  Serial.println("* Let's start PseudoAppKey join!");
   //! [SKT PseudoAppKey joining]
   LoRaWAN.setNetworkJoined(false);
   LoRaWAN.beginJoining(devEui, appEui, appKey);
   //! [SKT PseudoAppKey joining]
 #else
+  Serial.println("* Let's start RealAppKey join!");
   //! [SKT RealAppKey joining]
   LoRaWAN.setNetworkJoined(true);
   LoRaWAN.beginJoining(devEui, appEui, appKey);
   //! [SKT RealAppKey joining]
 #endif
 }
+#else
+static void taskBeginJoin(void *) {
+  Serial.stopListening();
+  Serial.println("* Let's start join!");
+  LoRaWAN.setCurrentDatarateIndex(1); //SF8
+  LoRaWAN.beginJoining(devEui, appEui, appKey);
+}
+#endif
 
 static void eventAppKeyInput(SerialPort &) {
   uint8_t numOctets = strlen(keyBuf);
-  if (numOctets % 2 == 0) {
+  if (numOctets == 32) {
     numOctets /= 2;
-    uint8_t *data = (uint8_t *) dynamicMalloc(numOctets);
-    if (data) {
-      char strOctet[3];
+    char strOctet[3];
 
-      for (uint8_t j = 0; j < numOctets; j++) {
-        strOctet[0] = keyBuf[2 * j];
-        strOctet[1] = keyBuf[2 * j + 1];
-        strOctet[2] = '\0';
+    for (uint8_t j = 0; j < numOctets; j++) {
+      strOctet[0] = keyBuf[2 * j];
+      strOctet[1] = keyBuf[2 * j + 1];
+      strOctet[2] = '\0';
 
-        data[j] = strtoul(strOctet, NULL, 16);
-      }
-
-      printf("* New AppKey:");
-      for (uint8_t j = 0; j < numOctets; j++) {
-        printf(" %02X", data[j]);
-      }
-      printf(" (%u byte)\n", numOctets);
-      ConfigMemory.write(data, 0, numOctets);
-      dynamicFree(data);
-      postTask(taskBeginJoin, NULL);
-      return;
-    } else {
-      printf("* Not enough memory\n");
+      appKey[j] = strtoul(strOctet, NULL, 16);
     }
+
+    printf("* New AppKey:");
+    for (uint8_t j = 0; j < numOctets; j++) {
+      printf(" %02X", appKey[j]);
+    }
+    printf(" (%u byte)\n", numOctets);
+    ConfigMemory.write(appKey, 0, numOctets);
+    postTask(taskBeginJoin, NULL);
+    return;
   } else {
-    printf("* HEX string length MUST be even number.");
+    printf("* HEX string length MUST be 32-byte.");
   }
 
   Serial.inputKeyboard(keyBuf, sizeof(keyBuf) - 1);
@@ -524,7 +633,10 @@ static void eventKeyInput(SerialPort &) {
 
   Serial.onReceive(eventAppKeyInput);
   Serial.inputKeyboard(keyBuf, sizeof(keyBuf) - 1);
-  Serial.println("* Enter a new appKey as a hexadecimal string [ex. 00112233445566778899aabbccddeeff]");
+  Serial.println(
+    "* Enter a new appKey as a hexadecimal string "
+    "[ex. 00112233445566778899aabbccddeeff]"
+  );
 }
 
 #endif //OVER_THE_AIR_ACTIVATION
@@ -532,14 +644,17 @@ static void eventKeyInput(SerialPort &) {
 void setup() {
   Serial.begin(115200);
   Serial.printf("\n*** [Nol.Board] LoRaWAN Class A&C Example ***\n");
+
   mysensor.begin();
   rht.begin(D13);
   pms3003.begin();
 
+  measurement.onFired(readSensor,NULL);
+  measurement.startPeriodic(60000);
   // Button to switch between class A and C.
+  pinMode(D9, INPUT);
+  attachInterrupt(D9, eventButtonPressed, FALLING);
 
-  test.onFired(air,NULL);
-  test.startPeriodic(300000);
   System.setTimeDiff(9 * 60);  // KST
 
   timerSend.onFired(taskPeriodicSend, NULL);
@@ -550,11 +665,13 @@ void setup() {
   LoRaWAN.onSendDone(eventLoRaWANSendDone);
   //! [How to set onSendDone callback]
 
-  //! [How to set onRecfeive callback]
+  //! [How to set onReceive callback]
   LoRaWAN.onReceive(eventLoRaWANReceive);
   //! [How to set onReceive callback]
 
+  //! [How to set onJoin callback]
   LoRaWAN.onJoin(eventLoRaWANJoin);
+  //! [How to set onJoin callback]
 
   //! [How to set onJoinRequested callback]
   LoRaWAN.onJoinRequested(eventLoRaWANJoinRequested);
@@ -592,14 +709,14 @@ void setup() {
 
   postTask(taskPeriodicSend, NULL);
 #else
-
+  // System.getEUI(devEui);
   Serial.printf(
     "* DevEUI: %02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X\n",
     devEui[0], devEui[1], devEui[2], devEui[3],
     devEui[4], devEui[5], devEui[6], devEui[7]
   );
 
-  ConfigMemory.read(appKey, 0, 16);
+  // ConfigMemory.read(appKey, 0, 16);
   Serial.printf(
     "* AppKey: %02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X\n",
     appKey[0], appKey[1], appKey[2], appKey[3],
@@ -612,7 +729,10 @@ void setup() {
     /* The appKey is required to be entered by user.*/
     Serial.onReceive(eventAppKeyInput);
     Serial.inputKeyboard(keyBuf, sizeof(keyBuf) - 1);
-    Serial.println("* Enter a new appKey as a hexadecimal string [ex. 00112233445566778899aabbccddeeff]");
+    Serial.println(
+      "* Enter a new appKey as a hexadecimal string "
+      "[ex. 00112233445566778899aabbccddeeff]"
+    );
   } else {
     Serial.println("* Press any key to enter a new appKey in 3 seconds...");
     timerKeyInput.onFired(taskBeginJoin, NULL);
@@ -624,5 +744,6 @@ void setup() {
 
 #endif //OVER_THE_AIR_ACTIVATION
 
-
+  pinMode(D4, OUTPUT);
+  digitalWrite(D4, LOW);
 }
